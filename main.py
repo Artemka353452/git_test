bc74e3462c29d533cc0f879632fd7ace8d86d71d
Artemka35345 <artem.samurai2005@gmail.com>
2022-12-12 13:09:26 +0300

    Добавили метод area

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ class Rectangle:
         self._a = a
         self._b = b
 
-
     def area(self):
         return self._a * self._b
 
@@ -13,14 +12,6 @@ class Rectangle:
     def get_width(self):
         return self._b
 
-def set_height(self, a):
-    self._a = a
-
-def set_width(self, b):
-    self._b = b
-
 
 main_rect = Rectangle(5, 4)
 print(main_rect.area())
-main_rect.set_height(7)
-main_rect.set_width(8)
