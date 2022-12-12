373541819adf3d326c15bb3a4200cfd515e3e523
Artemka35345 <artem.samurai2005@gmail.com>
2022-12-12 13:07:34 +0300

    Добавили метод area

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-class Circle:
-    def __init__(self, r):
-        self._r = r
+class Rectangle:
+    def __init__(self, a, b):
+        self._a = a
+        self._b = b
 
 
     def area(self):
@@ -19,7 +20,7 @@ def set_width(self, b):
     self._b = b
 
 
-main_circle = Circle(2)
+main_rect = Rectangle(5, 4)
 print(main_rect.area())
 main_rect.set_height(7)
 main_rect.set_width(8)
