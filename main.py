9a9b7c31ff69b175c20a6db152bc75c70ac2d6cf
Artemka35345 <artem.samurai2005@gmail.com>
2022-12-12 13:22:08 +0300

    sdsd

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from math import pi
 
 
+class Rectangle:
+    def __init__(self, a, b):
+        self._a = a
+        self._b = b
+
+    def area(self):
+        return self._a * self._b
+
+    def get_height(self):
+        return self._a
+
+    def get_width(self):
+        return self._b
+
+
 class Circle:
     def __init__(self, r):
         self._r = r
@@ -9,5 +24,9 @@ class Circle:
         return pi * (self._r ** 2)
 
 
+main_rect = Rectangle(5, 4)
+print(main_rect.area())
+main_rect.set_height(7)
+main_rect.set_width(8)
 main_circle = Circle(2)
 print(main_circle.area())
